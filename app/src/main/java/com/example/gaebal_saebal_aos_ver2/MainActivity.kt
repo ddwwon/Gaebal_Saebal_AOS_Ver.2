86dfdd44a46b7728c3c95d18d92c04b5fd1b3f05
Dowon <70639119+ddwwon@users.noreply.github.com>
2022-11-03 16:29:26 +0900

    2022/11/03 Navigation Bar 생성

--- app/src/main/java/com/example/gaebal_saebal_aos_ver2/MainActivity.kt
+++ app/src/main/java/com/example/gaebal_saebal_aos_ver2/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.gaebal_saebal_aos_ver2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
+import com.example.gaebal_saebal_aos_ver2.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(binding.fragmentLayout.id, MyRecordFragment()).commit()
     }
 }
